2cd206e6fc603008c4edfa2ac043e1fc9b73ebd6
BuildTools <unconfigured@null.spigotmc.org>
2022-11-02 14:12:37 +0100

    Add connection with drive

--- GoogleDriveStorage/src/main/java/gdstorage/GoogleDriveStorage.java
+++ GoogleDriveStorage/src/main/java/gdstorage/GoogleDriveStorage.java
@@ -1,5 +1,15 @@
 package gdstorage;
 
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.FileAlreadyExistsException;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
@@ -7,24 +17,43 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
 import storagecore.StorageCore;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.file.FileAlreadyExistsException;
-import java.util.Date;
-import java.util.List;
 
 public class GoogleDriveStorage extends StorageCore {
 
-    private static final String APPLICATION_NAME = "GoogleDriveSK";
+    /**
+     * Application name.
+     */
+    private static final String APPLICATION_NAME = "My project";
 
+    /**
+     * Global instance of the {@link FileDataStoreFactory}.
+     */
+    private static FileDataStoreFactory DATA_STORE_FACTORY;
 
+    /**
+     * Global instance of the JSON factory.
+     */
+    private static final JacksonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+
+    /**
+     * Global instance of the HTTP transport.
+     */
     private static HttpTransport HTTP_TRANSPORT;
 
+    /**
+     * Global instance of the scopes required by this quickstart.
+     * <p>
+     * If modifying these scopes, delete your previously saved credentials at
+     * ~/.credentials/calendar-java-quickstart
+     */
+    private static final List<String> SCOPES = Arrays.asList(DriveScopes.DRIVE);
 
     static {
         try {
@@ -34,10 +63,21 @@ public class GoogleDriveStorage extends StorageCore {
             System.exit(1);
         }
     }
-/*
+
+    public GoogleDriveStorage(String root) {
+        super(root);
+    }
+
+    /**
+     * Creates an authorized Credential object.
+     *
+     * @return an authorized Credential object.
+     * @throws IOException
+     */
     public static Credential authorize() throws IOException {
         // Load client secrets.
         InputStream in = GoogleDriveStorage.class.getResourceAsStream("/client_secret.json");
+
         GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
         // Build flow and trigger user authorization request.
@@ -52,20 +92,31 @@ public class GoogleDriveStorage extends StorageCore {
      *
      * @return an authorized Calendar client service
      * @throws IOException
-
+     */
     public static Drive getDriveService() throws IOException {
         Credential credential = authorize();
         return new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
                 .setApplicationName(APPLICATION_NAME)
                 .build();
     }
-*/
-    public GoogleDriveStorage(String root) {
-        super(root);
-    }
 
-    public GoogleDriveStorage(String root, int maxSizeLimit, List<String> bannedExtensions, int fileCountLimit) {
-        super(root, maxSizeLimit, bannedExtensions, fileCountLimit);
+    public static void main(String[] args) throws IOException {
+
+        Drive service = getDriveService();
+
+        FileList result = service.files().list()
+                .setPageSize(10)
+                .setFields("nextPageToken, files(id, name)")
+                .execute();
+        List<File> files = result.getFiles();
+        if (files == null || files.isEmpty()) {
+            System.out.println("No files found.");
+        } else {
+            System.out.println("Files:");
+            for (File file : files) {
+                System.out.printf("%s (%s)\n", file.getName(), file.getId());
+            }
+        }
     }
 
     @Override
@@ -132,5 +183,5 @@ public class GoogleDriveStorage extends StorageCore {
     public void searchByModifiedAfter(Date date) {
 
     }
-
 }
+
