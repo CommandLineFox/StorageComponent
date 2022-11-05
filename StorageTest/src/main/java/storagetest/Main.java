f447fc183238792e6a45bccb6f8fb905820b11f7
BuildTools <unconfigured@null.spigotmc.org>
2022-11-05 14:13:13 +0100

    Update Google Drive Storage(Config)

--- StorageTest/src/main/java/storagetest/Main.java
+++ StorageTest/src/main/java/storagetest/Main.java
@@ -14,43 +14,73 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        StorageCore storageCore = new LocalStorage("def");//Stavljeno kao default storage
+        StorageCore storageCore=null;
         System.out.println("Welcome, to storage");
         System.out.println("To create local storage input 1,to create google drive storage input 2");
         String input = "";
 
+        Scanner s = new Scanner(new InputStreamReader(System.in));
+        input = s.nextLine();
+        //Creating storage
+        if (input.equals("1") || input.equals("2")) {
+            System.out.println("Enter root directory(enter def for default directory):");
+            String directory = s.nextLine();
+
+            System.out.println("Enter maximum size(in bytes)");
+            int maxbytes = Integer.parseInt(s.nextLine());
+
+            System.out.println("Enter all banned extensions:");
+            List<String> bannedextensions = List.of(s.nextLine().split(" "));
+
+
+            if (input.equals("1")) {
+
+
+                //storageCore = new LocalStorage(directory, maxbytes, bannedextensions, filecount);
+            } else {
+                if(directory.equalsIgnoreCase("def"))
+                    directory="Root";
+
+                storageCore = new GoogleDriveStorage(directory, maxbytes, bannedextensions);
+
+            }
+        }
+        if(storageCore!=null)
         while (!input.equalsIgnoreCase("exit")) {
-            Scanner s = new Scanner(new InputStreamReader(System.in));
             input = s.nextLine();
             //Exit
             if (input.equalsIgnoreCase("exit")) {
-                continue;
+                break;
             }
 
-            //Creating storage
-            if (input.equals("1") || input.equals("2")) {
-                System.out.println("Enter root directory(enter def for default directory):");
-                String directory = s.nextLine();
+            if(input.split(" ")[0].equalsIgnoreCase("cd")){
+                List<String> niz = List.of(input.split(" "));
+                if (niz.size() == 2)//Create imefajla
+                {
+                    if(niz.get(1).equals(".."))//VRATI SE NAZAD
+                    {
+                        storageCore.returnBackFromDirectory();
 
-                System.out.println("Enter maximum size(in bytes)");
-                int maxbytes = Integer.parseInt(s.nextLine());
-
-                System.out.println("Enter all banned extensions:");
-                List<String> bannedextensions = List.of(s.nextLine().split(" "));
-
-                System.out.println("Enter file count limit:");
-                int filecount = Integer.parseInt(s.nextLine());
-
-                if (input.equals("1")) {
-
-
-                    storageCore = new LocalStorage(directory, maxbytes, bannedextensions, filecount);
-                } else {
-                    storageCore = new GoogleDriveStorage(directory, maxbytes, bannedextensions, filecount);
+                    }
+                    else//Idi u zadati direktorijum
+                    {
+                        storageCore.enterDirectory(niz.get(1));
+                    }
                 }
             }
-
             //Create directory
+            /*
+                                                Dozviljeno unositi
+
+                        create ime_fila                 -> Kreira file koji se zove ime_fila
+                        create 5-10                     -> Kreira 6 filova koji se zovu 5,6,...,10
+                        crete ime_fila ogranicenje      -> Prvo i ima ogranicen broj filova u sebi
+                        create ime_fila 5-10            -> Kreira 6 filova koji se zovu ime_fila5,..., ime_fila10
+
+
+             */
+
+
             else if (input.split(" ")[0].equalsIgnoreCase("create")) {
 
                 List<String> niz = List.of(input.split(" "));
@@ -58,12 +88,14 @@ public class Main {
                 {
                     try {
                         storageCore.createDirectory(niz.get(1));
+
                     } catch (FileAlreadyExistsException e) {
                         System.out.println("File already exists");
                         e.printStackTrace();
                     }
                 } else if (niz.size() == 3)//Create 1 20
                 {
+
                     try {
                         storageCore.createDirectory(Integer.parseInt(niz.get(1)), Integer.parseInt(niz.get(2)));
                     } catch (FileAlreadyExistsException e) {
@@ -83,15 +115,13 @@ public class Main {
             } else if (input.split(" ")[0].split(" ")[0].equalsIgnoreCase("addfile")) {
                 List<String> niz = List.of(input.split(" "));
                 if (niz.size() == 2) {
+
                     try {
                         storageCore.addFile(niz.get(1));
-                    } catch (FileNotFoundException e) {
-                        System.out.println("Invalid path");
-                        e.printStackTrace();
                     } catch (FileAlreadyExistsException e) {
-                        System.out.println("File already exists");
                         e.printStackTrace();
                     }
+
                 }
             } else if (input.split(" ")[0].split(" ")[0].equalsIgnoreCase("delete")) {
                 List<String> niz = List.of(input.split(" "));
